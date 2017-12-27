2e82acf7a403a08d698fa070a25e1ea4c69446f5
Abdulrahman Alzahrani <Abdulrahman.Alzahrani@morningstar.com>
2017-12-27 11:02:03 -0600

    Added Search Bar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import SearchBar from './components/searchBar';
+
 const API_KEY = 'AIzaSyAjcM7MXvVJm25MA62LxMWgsbsUtkBJ_zo';
 
 const App= () => {
-  return <div>LMFAO</div>;
+  return ( <div>
+    <SearchBar/>
+  </div>);
   }
   ReactDOM.render(<App/>, document.querySelector('.container'));
 
